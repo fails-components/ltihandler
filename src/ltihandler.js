e513685b18d43ca8c42e72aaf704a2092e3c6f6b
Marten Richter <marten.richter@tu-berlin.de>
2021-07-25 19:38:49 +0200

    Initial commit ltihandler

---
+++ src/ltihandler.js
@@ -0,0 +1,396 @@
+/*
+    Fails Components (Fancy Automated Internet Lecture System - Components)
+    Copyright (C)  2015-2017 (original FAILS), 
+                   2021- (FAILS Components)  Marten Richter <marten.richter@freenet.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { v4 as uuidv4,  validate } from 'uuid';
+import url from 'fast-url-parser';
+import  jwt from 'jsonwebtoken';
+import got from 'got';
+import  Jwk from 'rasha';
+import moment from 'moment';
+
+export class LtiHandler {
+    constructor(args) {
+        this.lmslist = args.lmslist;
+        this.redis = args.redis;
+        this.mongo =args.mongo;
+        this.signJwt = args.signJwt;
+        this.basefailsurl= args.basefailsurl;
+    }
+
+
+    handleLogin(req, res) {
+        //console.log("Request:", req);
+        // console.log("Res:",res);
+        const params = { ...req.body };
+        if (!params.iss || !params.login_hint || !params.target_link_uri)
+            return res.status(400).send({
+                status: 400, error: 'Bad Request',
+                details: { message: 'no login parameters' }
+            });
+
+        console.log("login request from", params.iss, "with client id", params.client_id); // may be remove later
+        let platform = this.lmslist[params.iss];
+        if (!platform) return res.status(400).send({ status: 400, error: 'Platform not registered!' });
+        // use client_id for getting data base connection
+        // if not active platform redirect
+
+
+        const query = {
+            response_type: 'id_token',
+            response_mode: 'form_post',
+            id_token_signed_response_alg: 'RS256',
+            scope: 'openid',
+            client_id: params.client_id,
+            redirect_uri: params.target_link_uri,
+            login_hint: params.login_hint,
+            nonce: uuidv4(),
+            prompt: 'none',
+            ///state: "blabla"  // ok we do not need this, since nothing is bound to an account in this system
+            // the lti is our authentification system, and identifies the user, so there is no access to our system
+            // on which we have to keep track
+            // it would be something else, if we require the user to login to our system first...
+        }
+        if (params.lti_message_hint) query.lti_message_hint = params.lti_message_hint
+        if (params.lti_deployment_id) query.lti_deployment_id = params.lti_deployment_id
+        res.redirect(url.format({
+            pathname: platform.auth_request_url,
+            query: query
+        }))
+    }
+
+    async handleLaunch(req, res) {
+        //console.log("Request:", req);
+        // console.log("Res:",res);
+        if (req.body.error) {
+            return res.send("LMS reported error: " + req.body.error_description);
+        }
+        if (req.body.id_token) {
+            const decoded_token = jwt.decode(req.body.id_token, { complete: true });
+            const platform = this.lmslist[decoded_token.payload.iss];
+            if (!platform) return res.status(400).send({
+                status: 400, error: 'Bad Request',
+                details: { message: 'platform not registered/supported' }
+            });
+
+            let keyinfo = await got.get(platform.keyset_url).json();
+            let keys = keyinfo.keys;
+            if (!keys) return res.status(400).send({ status: 400, error: 'Keyset not found' });
+
+            if (!decoded_token.header.kid)
+                return res.status(400).send({ status: 400, error: 'no valid kid!' });
+
+            const jwk = keys.find(key => { return key.kid === decoded_token.header.kid });
+            if (!jwk) return res.status(400).send({ status: 400, error: 'key not found' });
+            const key = await Jwk.export({ jwk: jwk });
+            let payload = decoded_token.payload;
+
+            //console.log("decoded token payload",payload);
+            if (!validate(payload.nonce)) return res.status(400).send({ status: 400, error: 'nonce wrong format' });
+            this.redis.exists("lti:nonce:" + payload.nonce, async (err, redres) => {
+                if (err) {
+                    console.log("redis error", err);
+                    return res.status(400).send({ status: 400, error: 'redis broken' });
+                } else if (redres == 1) return res.status(400).send({ status: 400, error: 'nonce reused, replay attack?' });
+                else {
+
+
+                    // we have to store it in the db
+                    this.redis.set("lti:nonce:" + payload.nonce, "dummy", 'EX', 60 * 10 /* 10 minutes*/); // we do not have to use the callback
+
+                    if (!jwt.verify(req.body.id_token, key, {/* nonce: ADD*/ }))
+                        return res.status(400).send({ status: 400, error: 'jwt verification failure' });
+
+                    if (payload['https://purl.imsglobal.org/spec/lti/claim/message_type'] !== 'LtiResourceLinkRequest')
+                        return res.status(400).send({status: 400, 
+                            error: "so far only resource links are supported " 
+                            + payload['https://purl.imsglobal.org/spec/lti/claim/message_type']});
+
+                    // now we have to collect the data
+                    let userinfo = {
+                        firstnames: payload.given_name,
+                        lastname: payload.family_name,
+                        displayname: payload.name,
+                        email: payload.email, //can be used for matching persons, multple possible
+                        lmssub: payload.sub  //even this may be missing in anonymus case
+                    }
+                    if (payload['https://purl.imsglobal.org/spec/lti/claim/ext'])
+                        userinfo.lmsusername =  payload['https://purl.imsglobal.org/spec/lti/claim/ext'].user_username;
+
+                    //console.log("userinfo", userinfo);
+                    let lmscontext = {
+                        // TODO add unique platform identifier?
+                        ret_url: payload['https://purl.imsglobal.org/spec/lti/claim/launch_presentation'].return_url,
+                        iss: payload.iss, //not optional, use for identification 
+
+                        /*aud: payload.aud,*/ //may be exclude
+                        platform_id: payload['https://purl.imsglobal.org/spec/lti/claim/tool_platform'].guid, //optional
+                        deploy_id: payload['https://purl.imsglobal.org/spec/lti/claim/deployment_id'], // do not use for identification, period!
+                        course_id: payload['https://purl.imsglobal.org/spec/lti/claim/context'].id, //optional, use for context identification if possible
+                        resource_id: payload['https://purl.imsglobal.org/spec/lti/claim/resource_link'].id, // use for identification
+
+                    }
+                    if ( !lmscontext.deploy_id
+                        || !lmscontext.resource_id || !lmscontext.iss) 
+                        return res.status(400).send({ status: 400, error: 'lti mandatory fields missing' });
+                    //console.log("lmscontext", lmscontext);
+                    let lectureinfo = {
+                        coursetitle: payload['https://purl.imsglobal.org/spec/lti/claim/context'].title ||
+                            payload['https://purl.imsglobal.org/spec/lti/claim/context'].label ||
+                            payload['https://purl.imsglobal.org/spec/lti/claim/resource_link'].title,
+                        lecturetitle: payload['https://purl.imsglobal.org/spec/lti/claim/resource_link'].title
+                    }
+                    //console.log("lectureinfo", lectureinfo);
+                    
+
+                    let role = [];
+                    let isinstructor=false;
+                    if (payload['https://purl.imsglobal.org/spec/lti/claim/roles'].includes('http://purl.imsglobal.org/vocab/lis/v2/membership#Learner'))
+                        role.push('audience');
+
+                    if (payload['https://purl.imsglobal.org/spec/lti/claim/roles'].includes('http://purl.imsglobal.org/vocab/lis/v2/membership#Instructor')){
+                        if (payload.sub) {
+                            role.push('instructor');
+                            isinstructor=true;
+                        }else role.push('audience'); //only audience supported, if anonymous
+                    }
+                    //console.log(role);
+                    // ok we have everything, but now we have to bind it to fails structures
+                    // we to identify lecture and context, which can be course or the lecture
+                    // and the user !!
+                    let failsuser= await this.identifyCreateUser(userinfo);
+                    console.log("failsuser",failsuser);
+                    let courseinfo={lms: lmscontext,
+                        linfo: lectureinfo};
+                    if (role.includes('instructor')) { 
+                        courseinfo.owner=failsuser.useruuid; //claim ownership
+                        courseinfo.ownerdisplayname=failsuser.displayname;
+                    }
+
+                    let failscourse=await this.identifyCreateLectureAndCourse(courseinfo); // TODO
+                    if (!failscourse) return res.status(400).send({ status: 400, error: 'resource can not be identified' });
+                    console.log("failscourse",failscourse);
+
+                    let token= {course: failscourse, user: failsuser, role: role, context:"lti", maxrenew: 5}; //five times 5 minutes should be enough
+                    let jwttoken=await this.signJwt(token);
+
+                    return res.redirect(this.basefailsurl+"?token="+jwttoken);
+
+                }
+
+                
+
+                res.send('LTI something failed');
+
+
+                //console.log("decoded token",decoded_token.payload);
+            });
+        } else return res.status(400).send({
+            status: 400, error: 'Bad Request',
+            details: { message: 'no id_token' }
+        });
+        
+
+
+    }
+
+    async identifyCreateUser(userinfo)
+    {
+        let userscol=this.mongo.collection("users");
+
+        let orquery=[];
+        if (userinfo.lmssub) orquery.push({"lms.sub": userinfo.lmssub });
+        if (userinfo.lmsusername) orquery.push({"lms.username": userinfo.lmsusername});
+        if (userinfo.email) orquery.push({email:userinfo.email});
+
+        if (orquery.length==0) return; // no user info get out
+
+        let userdoc=await userscol.findOne({ $or: orquery});
+        //console.log("user info from db", userdoc);
+        let useruuid;
+        let firstnames=userinfo.firstnames;
+        let lastname=userinfo.lastname;
+        let displayname=userinfo.displayname;
+        let email=userinfo.email;
+
+        if (userdoc == null) { // deploy data
+            let toinsert={};
+            if (!displayname && firstname && lastname) displayname=firstname+lastname;
+            if (displayname) toinsert.displayname=displayname;
+            if (userinfo.firstnames) toinsert.firstnames=userinfo.firstnames;
+            if (userinfo.lastname) toinsert.lastname=userinfo.lastname;
+            if (userinfo.email) toinsert.email=userinfo.email;
+            if (userinfo.lmssub || userinfo.lmsusername) toinsert.lms={};
+            if (userinfo.lmssub) {
+                toinsert.lms.sub=userinfo.lmssub;
+            }
+            if (userinfo.lmsusername) {
+                toinsert.lms.username=userinfo.lmsusername;
+            }
+            toinsert.lastlogin=new Date();
+            
+            useruuid=uuidv4();
+            toinsert.uuid=useruuid;
+
+            await userscol.insertOne(toinsert);
+
+           
+        } else {
+            //check if we want to update
+            useruuid= userdoc.uuid;
+            if (!firstnames) firstnames=userdoc.firstnames;
+            if (!lastname) lastname=userdoc.lastname;
+            if (!displayname) displayname=userdoc.displayname;
+            if (!email) email=userdoc.email;
+
+            if (!displayname && firstname && lastname) displayname=firstname+lastname;
+
+            let toupdate={};
+            if (userdoc.displayname!=displayname) toupdate.displayname=displayname;
+            if (userdoc.firstnames!=userinfo.firstnames) toupdate.firstnames=userinfo.firstnames;
+            if (userdoc.lastname!=userinfo.lastname) toupdate.lastname=userinfo.lastname;
+            if (userdoc.email!=userinfo.email) toupdate.email=userinfo.email;
+            if ((userinfo.lmssub || userinfo.lmsusername) 
+                    && (!userdoc.lms || userdoc.lms.sub!=userinfo.lmssub ||
+                        userdoc.lms.username!=userinfo.lmsusername)) {
+                let updatelms={};
+                if (userinfo.lmssub) updatelms.sub=userinfo.lmssub;
+                if (userinfo.lmsusername) updatelms.username=userinfo.lmsusername;
+                toupdate.lms=updatelms;
+            }
+            if (Object.keys(toupdate).length>0){
+                    userscol.updateOne({uuid: useruuid},
+                        {$set: toupdate, $currentDate: {lastlogin: true}});
+            } else {
+               // console.log("no update",toupdate);
+               if (!userdoc.lastlogin || (userdoc.lastlogin && moment(userdoc.lastlogin).isBefore(moment().subtract(3,'days')) )) {
+                   console.log("renew lastlogin")
+                    userscol.updateOne({uuid: useruuid},
+                            {$currentDate: {lastlogin: true}});
+                }
+            }
+           
+        }
+     
+        let retobj={useruuid: useruuid};
+        //if (firstnames) retobj.firstnames=firstnames;
+        //if (lastname) retobj.lastname=lastname;
+        if (displayname) retobj.displayname=displayname;
+        //if (email) retobj.email=email;
+        return retobj;
+    }
+
+    async identifyCreateLectureAndCourse(args)
+    {
+        var lms=args.lms;
+        var linfo=args.linfo;
+
+        let lecturescol=this.mongo.collection("lectures");
+
+        let andquery=[];
+
+        if (!lms.iss || !lms.resource_id) {
+            console.log("resource can not be identified! abort");
+            return null;
+        }
+        andquery.push({"lms.iss": lms.iss});
+        andquery.push({"lms.resource_id": lms.resource_id});
+
+        // TODO add course stuff
+        //console.log("andquery", andquery);
+        let lecturedoc=await lecturescol.findOne({$and: andquery});
+        console.log("lecturedoc", lecturedoc);
+
+        let lectureuuid=null;
+
+        let title=linfo.lecturetitle;
+        let coursetitle=linfo.coursetitle;
+
+        if (lecturedoc == null) { // deploy data
+            let toinsert={};
+            toinsert.lms={};
+            toinsert.lms.iss=lms.iss;
+            toinsert.lms.resource_id=lms.resource_id;
+            if (lms.course_id) toinsert.lms.course_id=lms.course_id;
+            if (lms.platform_id) toinsert.lms.platform_id=lms.platform_id;
+            if (linfo.lecturetitle) toinsert.title=linfo.lecturetitle;
+            if (linfo.coursetitle) toinsert.coursetitle=linfo.coursetitle;
+            lectureuuid=uuidv4();
+            toinsert.uuid=lectureuuid;
+            if (args.owner) {
+                toinsert.owners=[args.owner];
+                if (args.ownerdisplayname) toinsert.ownersdisplaynames=[args.ownerdisplayname];
+                else toinsert.ownersdisplaynames=["N.N."];
+                toinsert.date=new Date();
+                toinsert.lastaccess=new Date();
+
+            }
+           
+
+            await lecturescol.insertOne(toinsert);
+
+        } else {
+            if (!title) title=lecturedoc.title;
+            if (!coursetitle) coursetitle=lecturedoc.coursetitle;
+
+            lectureuuid=lecturedoc.uuid;
+            //check if we want to update
+            let toupdate={};
+            if (lecturedoc.lms.course_id!=lms.course_id|| lecturedoc.lms.platform_id!=lms.platform_id) toupdate.lms={};
+            if (lecturedoc.lms.course_id!=lms.course_id)  toupdate.lms.course_id=lms.course_id;
+            if (lecturedoc.lms.platform_id!=lms.platform_id)  toupdate.lms.platform_id=lms.platform_id;
+            if (lecturedoc.title!=linfo.lecturetitle) toupdate.title=linfo.lecturetitle;
+            if (lecturedoc.coursetitle!=linfo.coursetitle) toupdate.coursetitle=linfo.coursetitle;
+            let containsowner=true;
+            let isowner=false;
+            if (args.owner) {
+                containsowner=lecturedoc.owners.includes(args.owner);
+                isowner=true;
+            }
+
+            if (containsowner && !lecturedoc.date) toupdate.date= new Date();
+            if (Object.keys(toupdate).length>0 || !containsowner) {
+               let updateops={}; 
+               if (Object.keys(toupdate).length>0) updateops.$set=toupdate;
+               if (!containsowner && isowner) {
+                   updateops.$addToSet={owner: args.owner};
+                   if (args.ownerdisplayname) updateops.$push={ownersdisplaynames: args.ownerdisplayname};
+               }
+               if (isowner)  updateops.$currentDate= {lastaccess: true}; 
+               //console.log("toupdate",updateops);
+               lecturescol.updateOne({uuid: lectureuuid},updateops);
+            } else {
+                if (!lecturedoc.lastaccess || (lecturedoc.lastaccess && moment(lecturedoc.lastaccess).isBefore(moment().subtract(3,'days')) )) {
+                    if (isowner) {
+                        lecturescol.updateOne({uuid: lectureuuid},
+                             {$currentDate: {lastaccess: true}});
+                     }
+                 }
+            }
+
+        }
+        let retobj={lectureuuid: lectureuuid};
+        //if (title) retobj.title=title;
+        //if (coursetitle) retobj.coursetitle=coursetitle;
+
+        return retobj;
+        
+    }
+
+
+}
---
+++ src/server.js
@@ -0,0 +1,88 @@
+/*
+    Fails Components (Fancy Automated Internet Lecture System - Components)
+    Copyright (C)  2015-2017 (original FAILS), 
+                   2021- (FAILS Components)  Marten Richter <marten.richter@freenet.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import express from "express";
+import * as redis from "redis";
+import MongoClient from 'mongodb';
+import { FailsJWTSigner} from 'fails-components-security';
+import {FailsConfig } from 'fails-components-config';
+
+//import { v4 as uuidv4, validate as isUUID } from 'uuid';
+import { LtiHandler } from './ltihandler.js';
+
+
+
+let cfg = new FailsConfig();
+const redisclient = redis.createClient({ detect_buffers: true /* required by notescreen connection*/ });
+
+let mongoclient = await MongoClient.connect(cfg.getMongoURL(), { useNewUrlParser: true, useUnifiedTopology: true });
+let mongodb = mongoclient.db(cfg.getMongoDB());
+
+let appsecurity = new FailsJWTSigner({ redis: redisclient, type: 'app', expiresIn: "1m", secret: cfg.getKeysSecret() });
+
+
+const lms_list = cfg.getLmsList();
+
+var ltihandler = new LtiHandler({
+  lmslist: lms_list,
+  signJwt: appsecurity.signToken,
+  redis: redisclient, mongo: mongodb, basefailsurl: cfg.getURL('appweb')
+});
+
+var app = express();
+
+// may be move the io also inside the object, on the other hand, I can not insert middleware anymore
+
+/*var ioIns = new Server(server,{cors: {
+  origin: "http://192.168.1.116:3000",
+  methods: ["GET", "POST"],
+ // credentials: true
+}});*/
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+console.log("debug path ",cfg.getSPath('lti') + "/launch")
+app.all(cfg.getSPath('lti') + "/launch", (req, res) => {
+  return ltihandler.handleLaunch(req, res);
+});
+
+app.all(cfg.getSPath('lti') + "/login", (req, res) => {
+  return ltihandler.handleLogin(req, res);
+});
+
+
+/*
+// old test code?
+app.all("/auth",function(req,res,next) {
+  // console.log("Request:", req.token);
+  // console.log("Res:",res);
+   console.log("req.query auth",req.query);
+   console.log("req.body auth",req.body);
+   res.send('Hello LTI');
+ });
+ */
+
+
+
+
+app.listen(cfg.getPort('lti'), cfg.getHost(), function () {
+  console.log('Failsserver lti handler listening port:', cfg.getPort('lti'), ' host:', cfg.getHost());
+});
+
